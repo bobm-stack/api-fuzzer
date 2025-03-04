c20379bf93b296cdf0271d440ef2598df7331914
bobm-stack <bobleblob12345@gmail.com>
2025-03-04 23:54:57 +0100

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -3,11 +3,12 @@ import sys
 
 api = input("please enter the ip of the api:" ) 
 
-for word in sys.stdin:
-  discover = requests.get(url= api + f"/{word}")
-  if discover.status_code == 404:
-    loop()
-  else:
-    discovered = discover.json()
-    print(discovered)
+def fuzzer():
+  for word in sys.stdin:
+    discover = requests.get(url= api + f"/{word}")
+    if discover.status_code == 404:
+      fuzzer()
+    else:
+      discovered = discover.json()
+      print(discovered)
   
